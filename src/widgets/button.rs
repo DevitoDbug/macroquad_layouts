bc68275cf83db9aea1f24e55e5e91ca3f3960761
devitodbug <davidochiengy@gmail.com>
2026-07-04 15:52:52 +0300

    feat: add widget module and the button widget

---
+++ src/widgets/button.rs
@@ -0,0 +1,21 @@
+use macroquad::prelude::*;
+
+use crate::{drawable::Drawable, geometry::Bounds};
+
+struct Button {
+    pub bounds: Bounds,
+    pub background: Color,
+}
+
+impl Button {
+    pub fn new(bounds: Bounds, background: Color) -> Self {
+        Self { bounds, background }
+    }
+}
+
+impl Drawable for Button {
+    fn draw(&self, x: f32, y: f32) -> (f32, f32) {
+        draw_rectangle(x, y, self.bounds.width, self.bounds.height, self.background);
+        (self.bounds.get_max_width(), self.bounds.get_max_height())
+    }
+}
