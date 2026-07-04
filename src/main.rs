7709ad9cf9f35716e1215ed911dfde0c1c67d239
devitodbug <davidochiengy@gmail.com>
2026-07-04 18:02:22 +0300

    feat: update main function to include game loop and render sidebar

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,43 @@
 #![allow(dead_code)]
 #![allow(unused)]
 
-mod core;
+use macroquad::prelude::*;
 
-fn main() {
-    println!("Hello, world!");
+use crate::{
+    components::sidebar::Sidebar,
+    game::{WINDOW_HEIGHT, WINDOW_WIDTH},
+};
+
+mod components;
+mod core;
+mod game;
+
+#[macroquad::main(window_conf())]
+async fn main() {
+    loop {
+        clear_background(WHITE);
+        render_screen();
+
+        next_frame().await
+    }
+}
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Layout".to_string(),
+        window_width: game::WINDOW_WIDTH,
+        window_height: game::WINDOW_HEIGHT,
+        ..Default::default()
+    }
+}
+
+fn render_screen() {
+    let sidebar = Sidebar::new(
+        WINDOW_WIDTH as f32 - (WINDOW_WIDTH as f32 * 50. / 100.),
+        0.,
+        WINDOW_WIDTH as f32 * 60. / 100.,
+        WINDOW_HEIGHT as f32,
+        RED,
+    );
+    sidebar.render();
 }
