fa2fe784ad5b5e6ad9489f602ef3422d1eca3b13
devitodbug <davidochiengy@gmail.com>
2026-07-05 12:16:46 +0300

    fix: update draw method to accept mutable reference for Drawable trait

--- src/core/drawable/mod.rs
+++ src/core/drawable/mod.rs
@@ -1,3 +1,3 @@
 pub trait Drawable {
-    fn draw(&self, x: f32, y: f32) -> (f32, f32); // Returns the max for the x and y respectively
+    fn draw(&mut self, x: f32, y: f32) -> (f32, f32); // Returns the max for the x and y respectively
 }
