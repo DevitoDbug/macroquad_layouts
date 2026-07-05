fd0ef34dfaaeb6f6955e5f84a08632e85a4fad6f
devitodbug <davidochiengy@gmail.com>
2026-07-05 12:11:21 +0300

    feat: refactor Button struct to use height and width instead of bounds

--- src/core/widgets/button.rs
+++ src/core/widgets/button.rs
@@ -2,20 +2,26 @@ use macroquad::prelude::*;
 
 use crate::core::{drawable::Drawable, geometry::Bounds};
 
-struct Button {
-    pub bounds: Bounds,
+pub struct Button {
+    pub height: f32,
+    pub width: f32,
+
     pub background: Color,
 }
 
 impl Button {
-    pub fn new(bounds: Bounds, background: Color) -> Self {
-        Self { bounds, background }
+    pub fn new(height: f32, width: f32, background: Color) -> Self {
+        Self {
+            height,
+            width,
+            background,
+        }
     }
 }
 
 impl Drawable for Button {
-    fn draw(&self, x: f32, y: f32) -> (f32, f32) {
-        draw_rectangle(x, y, self.bounds.width, self.bounds.height, self.background);
-        (self.bounds.get_max_width(), self.bounds.get_max_height())
+    fn draw(&mut self, x: f32, y: f32) -> (f32, f32) {
+        draw_rectangle(x, y, self.width, self.height, self.background);
+        (self.width, self.height)
     }
 }
