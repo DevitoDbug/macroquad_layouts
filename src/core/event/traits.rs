1dd309e45241c054ea974a173cbd92732fdd5c72
devitodbug <davidochiengy@gmail.com>
2026-07-12 00:51:37 +0300

    chore: update handle_event method of the EventListener to use the self argument

--- src/core/event/traits.rs
+++ src/core/event/traits.rs
@@ -2,5 +2,5 @@ use crate::core::event::event::Event;
 
 pub trait EventListener {
     //TODO: Currently this is all i think the event handler should look like
-    fn handle_event(e: Event) -> bool;
+    fn handle_event(&self, e: Event) -> bool;
 }
