c558b7452d390cc0a3a3ed8015d8322250bb535b
devitodbug <davidochiengy@gmail.com>
2026-07-11 22:33:56 +0300

    feat: add the x y points for widgets, to help in future event handling chores

--- src/core/widgets/button.rs
+++ src/core/widgets/button.rs
@@ -3,6 +3,9 @@ use macroquad::prelude::*;
 use crate::core::{drawable::Drawable, geometry::Bounds};
 
 pub struct Button {
+    pub x: Option<f32>,
+    pub y: Option<f32>,
+
     pub height: f32,
     pub width: f32,
 
@@ -15,12 +18,18 @@ impl Button {
             height,
             width,
             background,
+            // x and y can only be set during drawing
+            x: None,
+            y: None,
         }
     }
 }
 
 impl Drawable for Button {
     fn draw(&mut self, x: f32, y: f32) -> (f32, f32) {
+        self.x = Some(x);
+        self.y = Some(y);
+
         draw_rectangle(x, y, self.width, self.height, self.background);
         (self.width, self.height)
     }
