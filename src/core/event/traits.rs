60dbbf3e09d5bacd4c5716a5c85d8618e542d72b
devitodbug <davidochiengy@gmail.com>
2026-07-12 22:30:55 +0300

    chore: remove unused trait file

--- src/core/event/traits.rs
+++
@@ -1,6 +0,0 @@
-use crate::core::event::event::Event;
-
-pub trait EventListener {
-    //TODO: Currently this is all i think the event handler should look like
-    fn handle_event(&self, e: Event) -> bool;
-}
