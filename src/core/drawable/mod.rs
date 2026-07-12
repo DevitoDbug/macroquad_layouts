0458580fe004a1835776b20e27f2d7b27838019d
devitodbug <davidochiengy@gmail.com>
2026-07-12 22:26:49 +0300

    chore: add the handle_event function signature to the drawable trait to make it easy for Drawable components to handle events

--- src/core/drawable/mod.rs
+++ src/core/drawable/mod.rs
@@ -1,3 +1,10 @@
+use macroquad::math::bool;
+
+use crate::core::event::event::Event;
+
 pub trait Drawable {
     fn draw(&mut self, x: f32, y: f32) -> (f32, f32); // Returns the max for the x and y respectively
+    fn handle_event(&self, e: &Event) -> Option<bool> {
+        None
+    }
 }
