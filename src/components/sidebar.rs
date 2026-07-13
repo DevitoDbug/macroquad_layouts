2ffffd8b1a476d4a685e19a47c0ec193e2605497
devitodbug <davidochiengy@gmail.com>
2026-07-13 21:32:34 +0300

    feat: refactor Sidebar to initialize root_ui in new method and update render logic

--- src/components/sidebar.rs
+++ src/components/sidebar.rs
@@ -16,16 +16,14 @@ use crate::{
 };
 
 pub struct Sidebar {
+    pub root_ui: Box<dyn Drawable>,
     pub background: Color,
+
     pub bounds: Bounds,
 }
 
 impl Sidebar {
     pub fn new(background: Color, bounds: Bounds) -> Self {
-        Self { background, bounds }
-    }
-
-    pub fn render(&self, events: &Vec<Event>) {
         let button1 = Box::new(Button::new(
             BLOCK_SIZE as f32 * 2.,
             BLOCK_SIZE as f32 * 5.,
@@ -48,28 +46,36 @@ impl Sidebar {
         ));
 
         let mut hor_layout = Box::new(HorizontalLayout::new(
-            self.bounds.height * 20. / 100.,
-            self.bounds.width,
+            bounds.height * 20. / 100.,
+            bounds.width,
             1.,
             1.,
             vec![button4, button3],
             YELLOW,
         ));
 
-        let mut vert_layout = VerticalLayout::new_with_pos(
-            self.bounds.x,
-            self.bounds.y,
-            self.bounds.height,
-            self.bounds.width,
-            self.bounds.gap,
-            self.bounds.padding,
+        let mut vert_layout = Box::new(VerticalLayout::new_with_pos(
+            bounds.x,
+            bounds.y,
+            bounds.height,
+            bounds.width,
+            bounds.gap,
+            bounds.padding,
             vec![button1, hor_layout, button2],
-            self.background,
-        );
+            background,
+        ));
 
-        vert_layout.draw(self.bounds.x, self.bounds.y);
+        Self {
+            root_ui: vert_layout,
+            background,
+            bounds,
+        }
+    }
+
+    pub fn render(&mut self, events: &Vec<Event>) {
+        self.root_ui.draw(self.bounds.x, self.bounds.y);
         for e in events {
-            vert_layout.handle_event(e);
+            self.root_ui.handle_event(e);
         }
     }
 }
