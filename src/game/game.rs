70ea29bfdca4dfa1b03ca25236bfc34bcc91bef3
devitodbug <davidochiengy@gmail.com>
2026-07-13 13:07:51 +0300

    feat: implement Game struct with event handling and layout rendering

---
+++ src/game/game.rs
@@ -0,0 +1,59 @@
+use macroquad::prelude::*;
+
+use crate::{
+    components::sidebar::Sidebar,
+    core::{
+        event::{event::Event, event_type::EventType},
+        geometry::Bounds,
+    },
+    game::{WINDOW_HEIGHT, WINDOW_WIDTH},
+};
+
+pub struct Game {}
+
+impl Game {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn start(&self) {
+        let mut events: Vec<Event> = vec![];
+
+        // Handle mouse events
+        let mouse_event = self.listen_to_mouse_event();
+        match mouse_event {
+            None => (),
+            Some(val) => events.push(val),
+        }
+
+        self.render_screen_layout(&events);
+    }
+
+    fn render_screen_layout(&self, events: &Vec<Event>) {
+        let game_panel_width = (WINDOW_WIDTH as f32 * 40. / 100.);
+        let sidebar_width = (WINDOW_WIDTH as f32 * 60. / 100.);
+        let sidebar = Sidebar::new(
+            BROWN,
+            Bounds {
+                x: WINDOW_WIDTH as f32 - sidebar_width,
+                y: 0.,
+                width: sidebar_width,
+                height: WINDOW_HEIGHT as f32,
+                gap: 2.0,
+                padding: 2.0,
+            },
+        );
+        sidebar.render(events);
+    }
+
+    fn listen_to_mouse_event(&self) -> Option<Event> {
+        if is_mouse_button_pressed(MouseButton::Left) {
+            let (x, y) = mouse_position();
+
+            let mouse_click_event = Event::new(EventType::Click, x, y);
+            return Some(mouse_click_event);
+        }
+
+        None
+    }
+}
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,3 +1,6 @@
+pub mod game;
+pub mod game_panel;
+
 pub const BLOCK_SIZE: i32 = 32;
 pub const WINDOW_WIDTH: i32 = BLOCK_SIZE * 10 + 400;
 pub const WINDOW_HEIGHT: i32 = BLOCK_SIZE * 30;
